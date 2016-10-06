fe1acd4aad2e7759dcc8ffe90cda1ce06bd58558
Liuhaoyan <854215903@qq.com>
2016-10-07 00:56:03 +0800

    文章详情代码

---
+++ application/views/blog_detail.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    < <meta charset="UTF-8">
+    <title>文章详情</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <base href="<?php echo site_url();?>">
+    <link rel="stylesheet"  type="text/css" href="<?php echo base_url();?>css/normalize.css">
+    <link rel="stylesheet"  type="text/css" href="<?php echo base_url();?>css/font-awesome.css">
+    <link rel="stylesheet" type="text/css"  href="<?php echo base_url();?>css/bootstrap.min.css">
+    <link rel="stylesheet"  type="text/css" href="<?php echo base_url();?>css/blog_detail.css">
+    <script>
+        window.addEventListener("load",function(){
+            setTimeout(hideURLbar,0);
+
+        },false);
+        function hideURLbar(){
+            window.scrollTo(0,1);
+        }
+    </script>
+</head>
+<body>
+    <?php inclide 'header.php'?>
+</body>
+</html>
